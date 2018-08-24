e86e94a082a1b34d4b874cf4624a94bdbfb48834
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 12:49:04 +0000

    automatically backed up by learn

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -3,8 +3,8 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
+    deli.each.with_index(1) do |person, x|
+      current_line << " #{x}. #{person}"
     end
     puts current_line
   end
